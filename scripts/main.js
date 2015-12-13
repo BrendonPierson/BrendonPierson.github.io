61f18943cb8aa10653d7656bd9eabd4008a038cb
Brendon Pierson <BrendonPierson@gmail.com>
2015-12-13 14:54:42 -0600

    styled mobile nav links

--- scripts/main.js
+++ scripts/main.js
@@ -3,11 +3,8 @@ var hamburger = document.getElementById("hamburger");
 hamburger.onclick = function() {
   var current = document.getElementById("mobileNav").style.display;
   if(current === "none"){
-    document.getElementById("mobileNav").style.display = "flex";
-    document.getElementById("mobileNav").className = "topNav-links-div";
+    document.getElementById("mobileNav").className = "topNav-links-div topNav-links-div-show";
   } else {
-    document.getElementById("mobileNav").style.display = "none";
-    document.getElementById("mobileNav").className = "";
-
+    document.getElementById("mobileNav").className = "topNav-links-div topNav-links-div-hide";
   }
 }
